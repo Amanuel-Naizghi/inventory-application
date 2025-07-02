2e846a2f055fe051e970ef5bafb0166a943ee8e6
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-07-02 15:49:00 -0600

    Setting up express project and creating new database has been completed

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/userRouter');
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+app.use('/',router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , ()=>console.log(`You are viewing you app from this port number ${PORT}`));
