7c613fc50870e97ec62d7cfa09b14e0d53aab75e
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-07-14 23:26:54 -0600

    Debug log added in pool

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 
+console.log("💡 Using DATABASE_URL:", process.env.DATABASE_URL);
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
